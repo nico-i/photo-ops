891f4020be387d33eb7f19cdc49cb0d9fdf92a3c
Nico Ismaili <nico.ismaili@aoe.com>
2024-05-26 13:30:21 +0100

    feat: add src dir to motif_service gateway, added proto to caption_service + restructuring
    /spend 15m

---
+++ services/caption_service/src/infrastructure/__generated__/python/v1/caption_service_pb2.pyi
@@ -0,0 +1,19 @@
+from google.api import annotations_pb2 as _annotations_pb2
+from messages.v1 import image_dto_pb2 as _image_dto_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetCaptionRequest(_message.Message):
+    __slots__ = ("image",)
+    IMAGE_FIELD_NUMBER: _ClassVar[int]
+    image: _image_dto_pb2.ImageDto
+    def __init__(self, image: _Optional[_Union[_image_dto_pb2.ImageDto, _Mapping]] = ...) -> None: ...
+
+class GetCaptionResponse(_message.Message):
+    __slots__ = ("caption",)
+    CAPTION_FIELD_NUMBER: _ClassVar[int]
+    caption: str
+    def __init__(self, caption: _Optional[str] = ...) -> None: ...
